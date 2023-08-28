bff93705e7a29585800ebb8a1c32cb4a6698c06d
오혜지 <50623145+fjdkslvn@users.noreply.github.com>
2023-08-28 23:21:47 +0900

    프로젝트 생성
    - 프로젝트 생성 및 git 연동

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  let name = "오혜지";
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>안녕 리액트.. 완강이 목표다... {name}</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
