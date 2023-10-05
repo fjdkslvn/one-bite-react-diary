0fe391e90c0e1fdfa25da0070c4deb18cc8a739e
오혜지 <50623145+fjdkslvn@users.noreply.github.com>
2023-10-05 22:16:27 +0900

    다이어리 리스트로 관리

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 
 import MyHeader from "./MyHeader";
 import MyFooter from "./MyFooter";
 import Counter from "./Counter";
 import Container from "./Container";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
+
+  const onRemove = (targetId) => {
+    const newDiaryList = data.filter((it) => it.id !== targetId);
+    setData(newDiaryList);
+  };
+
+  const onEdit = (targetId, newContent) => {
+    setData(
+      data.map((it) =>
+        it.id === targetId ? { ...it, content: newContent } : it
+      )
+    );
+  };
+
   return (
     <div className="App">
-      <DiaryEditor />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} onDelete={onRemove} onEdit={onEdit} />
     </div>
   );
 }
